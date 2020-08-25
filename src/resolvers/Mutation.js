228fe3d852f87b4532906163f133b8e218b7abba
carlos bolanos <cbolanos@hotwire.com>
2020-08-24 22:03:20 -0700

    cache and pagination

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,18 @@ const Mutations = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
         return item;
+    },
+    async updateItem(parent, args, ctx, info){
+        const updates = {...args};
+        delete updates.id;
+
+        return ctx.db.mutation.updateItem({ data: updates, where: {id: args.id} }, info);
+    },
+    async deleteItem(parent, args, ctx, info){
+        console.log('f: deleteItem args', args);
+        const where = {id: args.id};
+        const item = await ctx.db.query.item({where}, info);
+        return ctx.db.mutation.deleteItem({where}, info)
     }
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,9 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-    items: forwardTo('db')
+    items: forwardTo('db'),
+    itemsConnection: forwardTo('db'),
+    item: forwardTo('db')
 };
 
 module.exports = Query;
